ba15d9e9f2e1a0f6f911533aa8128063c5fae460
Swapnil Dey <137487949+Swapnil-github006@users.noreply.github.com>
2025-06-30 11:00:20 +0530

    Late Push

---
+++ Assests/js/app.js
@@ -0,0 +1,85 @@
+// ITEM SLIDER SECTION
+
+$(document).ready(function () {
+    $('.event-slider').slick({
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
+
+
+
+
+// FEEDBACK SLIDER
+
+// ITEM SLIDER SECTION
+
+$(document).ready(function () {
+    $('.feedback-slider').slick({
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        autoplay: false,
+        arrows: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
